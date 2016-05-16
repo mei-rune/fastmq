97be8ab6f81ce8839b5b43f56bc7f49737ef8016
runner.mei <runner.mei@gmail.com>
2016-05-16 22:44:35 +0800

    add unit test of message

--- server/message_test.go
+++ server/message_test.go
@@ -6,8 +6,41 @@ import (
 	"testing"
 )
 
+func assertEq(t *testing.T, reader *Reader, input string, excepted Message) {
+	msg, err := reader.ReadMessage()
+	if nil != err {
+		t.Error("[", input, "]", err)
+		return
+	}
+	if nil == msg {
+		msg, err = reader.ReadMessage()
+		if nil != err {
+			t.Error("[", input, "]", err)
+			return
+		}
+		if nil == msg {
+			t.Error("[", input, "] read is error")
+			return
+		}
+	}
+	if msg.Command() != excepted.Command() {
+		t.Error("[", input, "] command is error - ", msg.Command(), excepted.Command())
+		return
+	}
+	if msg.DataLength() != excepted.DataLength() {
+		t.Error("[", input, "] DataLength is error - ", msg.DataLength(), excepted.DataLength())
+		return
+	}
+
+	if !bytes.Equal(msg.Data(), excepted.Data()) {
+		//fmt.Println("'"+string(msg.ToBytes())+"'", len(msg.ToBytes()))
+		t.Error("[", input, "] Data is error - ", msg.Data(), excepted.Data())
+		return
+	}
+}
+
 func TestMessageReadLength(t *testing.T) {
-	for _, s := range []struct {
+	var ok_tests = []struct {
 		input    string
 		excepted Message
 	}{
@@ -24,45 +57,47 @@ func TestMessageReadLength(t *testing.T) {
 			excepted: NewMessageWriter('p', 0).Append(bytes.Repeat([]byte{'1'}, 1111)).Build()},
 		{input: "p 11111\n" + strings.Repeat("1", 11111),
 			excepted: NewMessageWriter('p', 0).Append(bytes.Repeat([]byte{'1'}, 11111)).Build()},
-	} {
+	}
 
+	for _, s := range ok_tests {
 		if !bytes.Equal(s.excepted.ToBytes(), []byte(s.input)) {
-			t.Error("[", s.input, "] Data is error - ", msg.Data(), s.excepted.Data())
+			t.Error("[", s.input, "] Data is error")
 			continue
 		}
 
-		//fmt.Println("'"+string(s.excepted.ToBytes())+"'", len(s.excepted.ToBytes()))
-		reader := NewMessageReader(strings.NewReader(s.input), 100)
-		msg, err := reader.ReadMessage()
-		if nil != err {
-			t.Error("[", s.input, "]", err)
-			continue
-		}
-		if nil == msg {
-			msg, err = reader.ReadMessage()
-			if nil != err {
-				t.Error("[", s.input, "]", err)
-				continue
-			}
-			if nil == msg {
-				t.Error("[", s.input, "] read is error")
-				continue
+		assertEq(t, NewMessageReader(strings.NewReader(s.input), 100), s.input, s.excepted)
+	}
+
+	for _, s := range ok_tests {
+		for i := 1; i < 10; i++ {
+			data := strings.Repeat(s.input, i)
+
+			rd := NewMessageReader(strings.NewReader(data), 100)
+			for j := 0; j < i; j++ {
+				assertEq(t, rd, s.input, s.excepted)
 			}
 		}
-		if msg.Command() != s.excepted.Command() {
-			t.Error("[", s.input, "] command is error - ", msg.Command(), s.excepted.Command())
-			continue
-		}
-		if msg.DataLength() != s.excepted.DataLength() {
-			t.Error("[", s.input, "] DataLength is error - ", msg.DataLength(), s.excepted.DataLength())
-			continue
-		}
+	}
+}
 
-		if !bytes.Equal(msg.Data(), s.excepted.Data()) {
-			//fmt.Println("'"+string(msg.ToBytes())+"'", len(msg.ToBytes()))
-			t.Error("[", s.input, "] Data is error - ", msg.Data(), s.excepted.Data())
-			continue
-		}
+func TestMessageReadMuti(t *testing.T) {
+	var ok_tests = []struct {
+		input    string
+		excepted Message
+	}{
+		//       123456789
+		{input: "p     0\np     0\n",
+			excepted: NewMessageWriter('p', 0).Build()},
+		{input: "p     1\n1p     1\n1",
+			excepted: NewMessageWriter('p', 0).Append([]byte{'1'}).Build()},
+		{input: "p    11\n11111111111p    11\n11111111111",
+			excepted: NewMessageWriter('p', 0).Append(bytes.Repeat([]byte{'1'}, 11)).Build()},
+	}
 
+	for _, s := range ok_tests {
+
+		rd := NewMessageReader(strings.NewReader(s.input), 100)
+		assertEq(t, rd, s.input, s.excepted)
+		assertEq(t, rd, s.input, s.excepted)
 	}
 }
